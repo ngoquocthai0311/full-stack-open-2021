4cfbdea3cdba8cbe497dcab0187c4ac394bc91ef
ngoquocthai0311 <ngoquocthai0311@gmail.com>
2021-10-22 11:20:13 +0700

    update part3 front-end build prod

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
+const cors = require('cors')
 const morgan = require('morgan')
 
 const app = express()
-
+app.use(express.static('build'))
+app.use(cors())
 app.use(express.json())
 
 morgan.token('data', (request, response) => {
