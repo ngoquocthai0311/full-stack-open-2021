d5602b95787927cd69078efcac4fcb7bd954888b
ngoquocthai0311 <ngoquocthai0311@gmail.com>
2021-11-03 22:43:02 +0700

    add app.js refacture index.js

---
+++ part4/app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const cors = require('cors')
+const logger = require('./utils/logger')
+const config = require('./utils/config')
+const blogRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.use(middleware.morganLog)
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (request, response) => {
+    response.send('<h1>Hello world</h1>')
+})
+app.use('/api/blogs', blogRouter)
+app.use(middleware.errorHandler)
+app.use(middleware.unknowEndPoint)
+
+module.exports = app
---
+++ part4/utils/middleware.js
@@ -0,0 +1,66 @@
+const morgan = require('morgan')
+
+morgan.token('data', (request, response) => {
+    const body = request.body
+    if (!body.title && !body.author && !body.url && !body.likes) {
+        return ''
+    }
+
+    const blogObj = {
+        title: body.title,
+        author: body.author,
+        url: body.url,
+        likes: body.likes
+    }
+    return JSON.stringify(blogObj)
+})
+
+const morganLog = morgan(function (tokens, req, res) {
+    return [
+        tokens.method(req, res),
+        tokens.url(req, res),
+        tokens.status(req, res),
+        tokens.res(req, res, 'content-length'), '-',
+        tokens['response-time'](req, res), 'ms',
+        tokens.data(req, res)
+    ].join(' ')
+})
+
+const errorHandler = (error, request, response, next) => {            
+    switch(error.name){
+        case 'CastError': {
+            response.status(400).json({
+                error: 'malformed'
+            })
+            break;
+        }
+        case 'ValidationError': {
+            response.status(400).json({
+                error: 'validation error'
+            })
+            break;
+        }       
+        case 'MongooseError': {
+            response.status(400).json({
+                error: 'time out'
+            })
+            break;
+        }
+        default: {
+            response.status(400).json({
+                error: error.message
+            })
+        }
+    }
+    next(error)
+}
+
+const unknowEndPoint = (request, response) => {
+    response.status(400).json({
+        error: 'unknown endpoint'
+    })
+}
+
+module.exports = {
+    morganLog, errorHandler, unknowEndPoint
+}
