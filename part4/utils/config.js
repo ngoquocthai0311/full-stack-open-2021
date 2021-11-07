6e22c57a428709df0011da17fbd102d0d48d5e1b
ngoquocthai0311 <ngoquocthai0311@gmail.com>
2021-11-07 15:49:28 +0700

    add supertest, cross-env, blog_api.test.js; update package.json

--- part4/utils/config.js
+++ part4/utils/config.js
@@ -1,9 +1,13 @@
+const { info } = require('../utils/logger')
 require('dotenv').config()
 
 /* global process*/
 const PORT = process.env.PORT
-const MONGODB_URI = process.env.MONGODB_URI
+const MONGODB_URI = process.env.NODE_ENV === 'test'
+    ? process.env.TEST_MONGODB_URI
+    : process.env.MONGODB_URI
 
+info('current mongodb uri is' ,MONGODB_URI)
 module.exports = {
     PORT, MONGODB_URI
 }
