38de3330e6365a9bc26b47cfe688de0a67e5e2d0
ngoquocthai0311 <ngoquocthai0311@gmail.com>
2021-11-07 16:38:01 +0700

    refactor blogRouter, blog_api.test.js; add test_helpers, update config.js

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,25 +1,32 @@
 const blogRouter = require('express').Router()
 const Blog = require('../models/Blog')
 
-blogRouter.get('/', (request, response) => {
-    Blog
-        .find({})
-        .then(blogs => {
-            response.json(blogs)
-        })
+blogRouter.get('/', async (request, response, next) => {
+    try {
+        const blogs = await Blog.find({})
+        response.json(blogs)
+    } catch(error) {
+        next(error)
+    }
 })
 
-blogRouter.post('/', (request, response, next) => {
+blogRouter.post('/', async (request, response, next) => {
     const blog = new Blog(request.body)
 
-    blog
-        .save()
-        .then(result => {
-            response.status(201).json(result)
-        })
-        .catch(error => {
-            next(error)
-        })
+    try {
+        const savedBlog = await blog.save()
+        response.status(201).json(savedBlog)
+    } catch(error)  {
+        next(error)
+    }
+    // blog
+    //     .save()
+    //     .then(result => {
+    //         response.status(201).json(result)
+    //     })
+    //     .catch(error => {
+    //         next(error)
+    //     })
 })
 
 module.exports = blogRouter
--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -2,41 +2,42 @@ const mongo = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
 const Blog = require('../models/Blog')
-
-const initalBlogs = [
-    {
-        _id: '5a422a851b54a676234d17f7',
-        title: 'React patterns',
-        author: 'Michael Chan',
-        url: 'https://reactpatterns.com/',
-        likes: 7,
-        __v: 0
-    },
-    {
-        _id: '5a422aa71b54a676234d17f8',
-        title: 'Go To Statement Considered Harmful',
-        author: 'Edsger W. Dijkstra',
-        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-        likes: 5,
-        __v: 0
-    },
-]
+const helper = require('../utils/test_helpers')
 
 beforeEach(async () => {
     await Blog.deleteMany({})
-    let blogObject = new Blog(initalBlogs[0])
-    await blogObject.save()
-    blogObject = new Blog(initalBlogs[1])
-    await blogObject.save()
+
+    // Promise.all() executes promises in parallel
+    /*
+    const blogObjects = helper.initalBlogs.map(blog => new Blog(blog))
+    const promiseBlogsArray = blogObjects.map(blog => blog.save())
+    await Promise.all(promiseBlogsArray)
+    */
+
+    // for of block can execute promises in order
+    for (let each of helper.initalBlogs) {
+        let blog = new Blog(each)
+        await blog.save()
+    }
 })
 
 const api = supertest(app)
 
-test('blog are returned as json', async () => {
-    await api
-        .get('/api/blogs')
-        .expect(200)
-        .expect('Content-Type', /application\/json/)
+describe('get request', () => {
+    test('blog are returned as json', async () => {
+        await api
+            .get('/api/blogs')
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+    })
+    test('there are two blogs', async() => {
+        const response = await api.get('/api/blogs')
+        expect(response.body).toHaveLength(helper.initalBlogs.length)
+    })
+    test('the first blog has the title React patterns', async () => {
+        const response = await api.get('/api/blogs')
+        expect(response.body[0].title).toBe('React patterns')
+    })
 })
 
 afterAll(() => {
