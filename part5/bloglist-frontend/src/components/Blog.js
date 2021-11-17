01896dbf0a8b23ac0f8c2c6c81badc8dcc6ebd39
ngoquocthai0311 <ngoquocthai0311@gmail.com>
2021-11-17 18:09:13 +0700

    add solution 5.7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,32 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
+import React, { useState } from 'react'
+
+const Blog = ({blog}) => {
+  const [visibility, setVisibility] = useState(false)
+
+  const hideWhenVisibile = { display: visibility ? 'none' : '' }
+  const showWhenVisible = { display: visibility ? '' : 'none'}
+
+  const toggleVisibility = () => {
+    setVisibility(!visibility)
+  }
+
+  const inlineStyle = {
+    border: '2px solid black',
+    padding: '10px 0px 0px 0px',
+    margin: '0px 0px 10px 0px'
+  }
+  return (
+  <div style={inlineStyle}>
+    <div style={hideWhenVisibile}>
+      {blog.title} <button onClick={toggleVisibility}>view</button>
+    </div>
+    <div style={showWhenVisible}>
+      {blog.title} <button onClick={toggleVisibility}>hide</button> <br />
+      {blog.url} <br />
+      {blog.likes} <button>like</button> <br />
+      {blog.author} <br />
+    </div>
   </div>  
-)
+)}
 
 export default Blog
